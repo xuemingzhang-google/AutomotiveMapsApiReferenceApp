c17a3e3d7e5084604d75df0636a9a0fe0f8a333e
Xueming(Robert) Zhang <xuemingzhang@google.com>
2023-06-17 16:07:22 -0700

    Update to use application default credentials

--- src/test/java/cloudcode/helloworld/web/HelloWorldControllerTests.java
+++ src/test/java/cloudcode/helloworld/web/HelloWorldControllerTests.java
@@ -5,6 +5,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 import org.junit.Test;
+import org.junit.Rule;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -12,6 +13,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.junit.contrib.java.lang.system.EnvironmentVariables;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -20,8 +22,14 @@ public class HelloWorldControllerTests {
 
   @Autowired private MockMvc mvc;
 
+  // @Rule
+  // public final EnvironmentVariables environmentVariables
+  //     = new EnvironmentVariables();
+
   @Test
   public void getIndexView() throws Exception {
+    // environmentVariables.set("GOOGLE_APPLICATION_CREDENTIALS", "application_default_credentials.json");
+
     mvc.perform(MockMvcRequestBuilders.get("/"))
         .andExpect(status().isOk())
         .andExpect(view().name("index"))
